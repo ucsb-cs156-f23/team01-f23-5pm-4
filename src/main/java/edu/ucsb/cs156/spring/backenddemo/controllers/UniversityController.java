868e193885f78ff0096cbaf9f77c955cdba9cf5b
Francisco Leyva <leyvajustin@rocketmail.com>
2023-10-19 14:45:20 -0700

    ANOTHER ATTEMPT

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityController.java
@@ -1,4 +1,5 @@
 package edu.ucsb.cs156.spring.backenddemo.controllers;
+
 import edu.ucsb.cs156.spring.backenddemo.services.UniversityQueryService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,20 +16,20 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-
 @RestController
-@Tag(name="University info from universities.hipolabs.com")
+@Tag(name = "University info from universities.hipolabs.com")
 @Slf4j
 @RequestMapping("/api/university")
 public class UniversityController {
     @Autowired
     UniversityQueryService UniversityQueryService;
+
     @Operation(summary = "Get list of universities that match a given name", description = "Uses API documented here: http://universities.hipolabs.com/search")
     @GetMapping("/get")
     public ResponseEntity<String> getUniversity(
-        @Parameter(name="Name", description="name to search", example="Harvard or Stanford") @RequestParam String name
-    ) throws JsonProcessingException {
-        log.info("getUniversity: {}", name);
+            @Parameter(name = "Name", description = "name to search", example = "Harvard or Stanford") @RequestParam String name)
+            throws JsonProcessingException {
+        log.info("getUniversity: name = {}", name);
         String result = UniversityQueryService.getJSON(name);
         return ResponseEntity.ok().body(result);
     }
