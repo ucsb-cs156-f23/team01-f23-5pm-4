acf62dc0799aca9b10ad9783f9754db8341284dd
Francisco Leyva <leyvajustin@rocketmail.com>
2023-10-24 12:01:23 -0700

    Update UniversityControllerTests.java

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityControllerTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.http.MediaType;
 
 import edu.ucsb.cs156.spring.backenddemo.services.UniversityQueryService;
 
@@ -20,12 +19,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class UniversityControllerTests {
     @Autowired
     private MockMvc mockMvc;
-    
     @MockBean
     UniversityQueryService mockUniversityQueryService;
 
     @Test
-    public void test_getCountryCodes_validResponse() throws Exception {
+    public void test_getCountryCodes() throws Exception {
+
         String fakeJsonResult = "{ \"fake\" : \"result\" }";
         String name = "Harvard";
         when(mockUniversityQueryService.getJSON(eq(name))).thenReturn(fakeJsonResult);
@@ -33,37 +32,12 @@ public class UniversityControllerTests {
         String url = String.format("/api/university/get?name=%s", name);
 
         MvcResult response = mockMvc
-                .perform(get(url).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andReturn();
+                .perform(get(url).contentType("application/json"))
+                .andExpect(status().isOk()).andReturn();
 
         String responseString = response.getResponse().getContentAsString();
+
         assertEquals(fakeJsonResult, responseString);
     }
-    
-    @Test
-    public void test_getCountryCodes_invalidName() throws Exception {
-        String name = "";
-        String expectedMessage = "Invalid university name";
 
-        String url = String.format("/api/university/get?name=%s", name);
-
-        mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(content().string(expectedMessage));
-    }
-
-    @Test
-    public void test_getCountryCodes_serviceError() throws Exception {
-        String name = "Harvard";
-        when(mockUniversityQueryService.getJSON(eq(name))).thenThrow(new RuntimeException("Service error"));
-
-        String url = String.format("/api/university/get?name=%s", name);
-
-        mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isInternalServerError())
-                .andExpect(content().string("Service error"));
-    }
-
-}
+} 
