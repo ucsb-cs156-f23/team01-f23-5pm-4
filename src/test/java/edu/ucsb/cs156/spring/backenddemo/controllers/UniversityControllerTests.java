3a479157ff6e3a9078c99634e2e3e133766c434d
Francisco Leyva <leyvajustin@rocketmail.com>
2023-10-24 12:21:42 -0700

    Update UniversityControllerTests.java

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/UniversityControllerTests.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.client.HttpClientErrorException;
 
 import edu.ucsb.cs156.spring.backenddemo.services.UniversityQueryService;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -40,4 +43,18 @@ public class UniversityControllerTests {
         assertEquals(fakeJsonResult, responseString);
     }
 
-} 
+    @Test
+public void test_getUniversity_notFound() throws Exception {
+    when(mockUniversityQueryService.getJSON(anyString())).thenThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND));
+
+    String url = "/api/university/get?name=UnknownUniversity";
+    mockMvc.perform(get(url).contentType("application/json"))
+            .andExpect(status().isNotFound());
+}
+
+
+
+
+
+
+}
